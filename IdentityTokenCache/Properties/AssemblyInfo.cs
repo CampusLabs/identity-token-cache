3739c55919377451d09c46939c75a397957fb0de
Sean Townsend <sean.townsend@studentvoice.com>
2016-06-21 13:50:46 -0400

    add supporting nuget files

--- IdentityTokenCache/Properties/AssemblyInfo.cs
+++ IdentityTokenCache/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IdentityTokenCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Identity Foundation Token Caches")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Campus Labs")]
 [assembly: AssemblyProduct("IdentityTokenCache")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
